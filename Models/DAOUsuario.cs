97519188b96a0c4608975909f04df61f4d75ee58
Alander Machado <alandermm@hotmail.com>
2018-01-21 19:59:54 -0200

    Listar Usuários OK

--- Models/DAOUsuario.cs
+++ Models/DAOUsuario.cs
@@ -1,7 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
 namespace Forum.Models
 {
     public class DAOUsuario
     {
-        
+        SqlConnection con = null;
+
+        SqlCommand cmd = null;
+
+        SqlDataReader rd = null;
+
+        string conexao = @"Data Source = .\SQLEXPRESS; initial catalog = Forum; user id = sa; password=senai@123";
+
+        public List<Usuario> Listar(){
+            List<Usuario> usuarios = new List<Usuario>();
+
+            try{
+                con = new SqlConnection();
+                con.ConnectionString = conexao;
+                con.Open();
+                cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from usuario";
+                rd = cmd.ExecuteReader();
+                while(rd.Read()){
+                    usuarios.Add(new Usuario(){
+                        Id = rd.GetInt32(0),
+                        Nome = rd.GetString(1),
+                        Login = rd.GetString(2),
+                        Senha = rd.GetString(3),
+                        DataCadastro = rd.GetDateTime(4)
+                    });
+                }
+            }catch (SqlException se){
+                throw new Exception(se.Message);
+            } catch (Exception e){
+                throw new Exception(e.Message);
+            } finally {
+                con.Close();
+            }
+            return usuarios;
+        }
     }
 }
