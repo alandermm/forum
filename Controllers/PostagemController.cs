a23cfbc2bf9437113274eb56f5244c3f9b623285
Alander Machado <alandermm@hotmail.com>
2018-01-21 22:22:47 -0200

    Rotas para Listar, Cadastrar, Editar e Apagar Postagens OK

--- Controllers/PostagemController.cs
+++ Controllers/PostagemController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Forum.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers
@@ -5,6 +8,36 @@ namespace Forum.Controllers
     [Route("postagem/[controller")]
     public class PostagemController:Controller
     {
+        Postagem postagem = new Postagem();
+        DAOPostagem dao = new DAOPostagem();
+        
+        [HttpGet(Name = "Postagens")]
+        public IEnumerable<Postagem> Get(){
+            return dao.Listar();
+        }
+
+        [HttpGet("{id}", Name="PostagemAtual")]
+        public Postagem GetPostagem(int id){
+            return dao.Listar().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Postagem postagem){
+            dao.Cadastrar(postagem);
+            return CreatedAtRoute("PostagemAtual", new{id = postagem.Id}, postagem);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Postagem postagem){
+            dao.Editar(postagem);
+            return CreatedAtRoute("PostagemAtual", new{id = postagem.Id}, postagem);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id){
+            dao.Apagar(id);
+            return CreatedAtRoute("Postagens", null, null);
+        }
         
     }
 }
